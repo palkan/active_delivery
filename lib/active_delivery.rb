78fac7a3072b9122491b0d3d9007965709fc292b
ta1kt0me <1412864+ta1kt0me@users.noreply.github.com>
2025-07-31 11:28:25 +0900

    fix: match class and file names

--- lib/active_delivery.rb
+++ lib/active_delivery.rb
@@ -11,7 +11,7 @@ require "active_delivery/callbacks" if defined?(ActiveSupport)
 require "active_delivery/lines/base"
 require "active_delivery/lines/mailer" if defined?(ActionMailer)
 
-require "active_delivery/raitie" if defined?(::Rails::Railtie)
+require "active_delivery/railtie" if defined?(::Rails::Railtie)
 require "active_delivery/testing" if ENV["RACK_ENV"] == "test" || ENV["RAILS_ENV"] == "test"
 
 require "abstract_notifier"
